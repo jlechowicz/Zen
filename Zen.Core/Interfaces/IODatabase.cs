e49907d7e97b542d4f2486bbc3ebdbd27e62b3e3
John Lechowicz <john.lechowicz@accenture.com>
2016-03-16 09:50:03 -0500

    Added 3 tests

---
+++ Zen.Core/Interfaces/IODatabase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zen.Core.Interfaces
+{
+    public interface IODatabase
+    {
+        string ServerAddress { get; set; }
+        string Name { get; set; }
+        int Port { get; set; }
+        int Connect(string username, string password);
+        IOClass[] GetClasses();
+        object[] GetUsers();
+        object[] GetRoles();
+    }
+}
---
+++ Zen.Test/DatabaseTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zen.Core.Interfaces;
+using NSubstitute;
+
+namespace Zen.Test
+{
+    [TestClass]
+    public class DatabaseTests
+    {
+        [TestMethod]
+        public void ConnectToDatabaseTest()
+        {
+            var database = Substitute.For<IODatabase>();
+            string username = string.Empty;
+            string password = string.Empty;
+            database.Connect(username, password).Returns(1);
+            int result = database.Connect(username, password);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void InvalidLoginTest()
+        {
+            var database = Substitute.For<IODatabase>();
+            string username = "username";
+            string password = "invalidpassword";
+            database.Connect(username, password).Returns(0);
+            int result = database.Connect(username, password);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void GetClassesTest()
+        {
+            var database = Substitute.For<IODatabase>();
+            string username = string.Empty;
+            string password = string.Empty;
+            database.Connect(username, password).Returns(1);
+            database.GetClasses().Returns(new IOClass[] { });
+            IOClass[] classes = database.GetClasses();
+            Assert.AreEqual(0, classes.Length);
+        }
+    }
+}
