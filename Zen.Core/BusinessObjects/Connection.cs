a3ff96fd323ff81b9b3191e0258b260d08cd0bc1
John Lechowicz <john.lechowicz@accenture.com>
2016-03-17 17:16:23 -0500

    Added Connection business object

---
+++ Zen.Core/BusinessObjects/Connection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orient.Client;
+using Zen.Core.Interfaces;
+
+namespace Zen.Core.BusinessObjects
+{
+    public class Connection : IConnection
+    {
+        private readonly int DEFAULT_POOL_SIZE = int.Parse(System.Configuration.ConfigurationManager.AppSettings.Get("DefaultConnectionPoolSize"));
+        private readonly int DEFAULT_PORT = int.Parse(System.Configuration.ConfigurationManager.AppSettings.Get("DefaultConnectionPoolSize"));
+
+        private IZClient _client;
+        public Connection(IZClient client)
+        {
+            _client = client;
+        }
+
+        public void OpenPool(string hostname, int port, string databaseName, ODatabaseType databaseType, string username, string password, string alias)
+        {
+            _client.CreateDatabasePool(hostname, port, databaseName, databaseType, username, password, DEFAULT_POOL_SIZE, alias);
+        }
+
+        public void ClosePool(string alias)
+        {
+            _client.DropDatabasePool(alias);
+        }
+
+        public void OpenPool(string hostname, string databaseName, ODatabaseType databaseType, string username, string password, string alias)
+        {
+            _client.CreateDatabasePool(hostname, DEFAULT_PORT, databaseName, databaseType, username, password, DEFAULT_POOL_SIZE, alias);
+        }
+    }
+}
---
+++ Zen.Core/Interfaces/IConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zen.Core.Interfaces
+{
+    public interface IConnection
+    {
+        void OpenPool(string hostname, string databaseName, Orient.Client.ODatabaseType databaseType, string username, string password, string alias);
+        void OpenPool(string hostname, int port, string databaseName, Orient.Client.ODatabaseType databaseType, string username, string password, string alias);
+        void ClosePool(string alias);
+    }
+}
