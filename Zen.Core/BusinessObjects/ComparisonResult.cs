0a4cd62e46436cb73f5dbb697a054a2400c1731c
John Lechowicz <john.lechowicz@accenture.com>
2016-03-18 17:55:35 -0500

    Added ComparisonResult enum

---
+++ Zen.Core/BusinessObjects/ComparisonResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zen.Core.BusinessObjects
+{
+    public enum ComparisonResult
+    {
+        NotFoundInDestination,
+        NotFoundInSource,
+        IsEqual
+    }
+}
