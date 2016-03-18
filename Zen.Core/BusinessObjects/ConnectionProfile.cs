9879749072f068f1435049a5ef8676eeb7241d05
John Lechowicz <john.lechowicz@accenture.com>
2016-03-18 18:11:47 -0500

    Added ConnectionProfile

---
+++ Zen.Core/BusinessObjects/ConnectionProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zen.Core.BusinessObjects
+{
+    public class ConnectionProfile
+    {
+        public string ProfileName { get; set; }
+        public string HostName { get; set; }
+        public int Port { get; set; }
+        public string DatabaseName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
