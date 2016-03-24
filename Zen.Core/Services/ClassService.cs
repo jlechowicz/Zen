8ba600818cd19220fc97d397f6b99633bec658ab
John Lechowicz <john.lechowicz@accenture.com>
2016-03-24 11:48:17 -0500

    Added class and property services

---
+++ Zen.Core/Services/ClassService.cs
@@ -0,0 +1,24 @@
+﻿using Orient.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zen.Core.Entities;
+using Zen.Core.Interfaces;
+
+namespace Zen.Core.Services
+{
+    public class ClassService : IClassService
+    {
+        public IZClass[] GetClassesFromDatabase(IZDatabase database)
+        {
+            string query = "select expand(classes) from metadata:schema";
+            var result = database.Command(query).ToSingle();
+            var schema = result.GetField<List<ODocument>>("Content");
+            var classNames = schema.Select(d => d.GetField<string>("name"));
+            var classes = classNames.Select(n => new ZClass { Name = n }).ToArray();
+            return classes;
+        }
+    }
+}
---
+++ Zen.Core/Services/PropertyService.cs
@@ -0,0 +1,24 @@
+﻿using Orient.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zen.Core.Entities;
+using Zen.Core.Interfaces;
+
+namespace Zen.Core.Services
+{
+    public class PropertyService : IPropertyService
+    {
+        public IZProperty[] GetPropertiesFromClass(IZDatabase database, string className)
+        {
+            string query = string.Format("select expand(properties) from (select expand(classes) from metadata:schema) where name = '{0}'", className);
+            var result = database.Command(query).ToSingle();
+            var schema = result.GetField<List<ODocument>>("Content");
+            var propertyNames = schema.Select(d => d.GetField<string>("name"));
+            var properties = propertyNames.Select(n => new ZProperty { PropertyName = n, ValueType = OType.Null }).ToArray();
+            return properties;
+        }
+    }
+}
