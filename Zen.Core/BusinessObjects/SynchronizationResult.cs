24b1158c662b2f76ea13a6f6fc00a8010bd9e93a
John Lechowicz <john.lechowicz@accenture.com>
2016-03-25 17:01:23 -0500

    Added synchronization engine

---
+++ Zen.Core/BusinessObjects/SynchronizationResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zen.Core.BusinessObjects
+{
+    public class SynchronizationResult
+    {
+        public bool IsSuccessful { get; set; }
+        public SynchronizationDirection Direction { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ Zen.Core/Interfaces/ISynchronizationEngine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zen.Core.BusinessObjects;
+
+namespace Zen.Core.Interfaces
+{
+    public interface ISynchronizationEngine
+    {
+        IComparisonEngine ComparisonEngine { get; set; }
+        SynchronizationResult[] SyncObjects();
+    }
+}
